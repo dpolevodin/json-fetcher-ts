5608159993bb1dbe519594f707ae3f9b8783ff43
Dima <dpolevodin@gmail.com>
2022-02-09 13:22:16 +0500

    refactor

--- index.ts
+++ index.ts
@@ -3,5 +3,15 @@ import axios from 'axios'
 const url = 'https://jsonplaceholder.typicode.com/todos/1'
 
 axios.get(url).then(response => {
-  console.log(response.data)
+  const toDo = response.data
+  const id = toDo.id
+  const title = toDo.title
+  const completed = toDo.completed
+
+  console.log(`
+    The Todo with id: ${id}
+    Has a title of: ${title}
+    Is it completed? ${completed ? 'yes' : 'no'}
+  `)
 })
+
