082b379ee8f51aadac1e45d1a86e7198d365599c
Dima <dpolevodin@gmail.com>
2022-02-10 17:32:03 +0500

    added variables

--- index.js
+++ index.js
@@ -3,5 +3,12 @@ exports.__esModule = true;
 var axios_1 = require("axios");
 var url = 'https://jsonplaceholder.typicode.com/todos/1';
 axios_1["default"].get(url).then(function (response) {
-    console.log(response.data);
+    var toDo = response.data;
+    var id = toDo.id;
+    var title = toDo.title;
+    var completed = toDo.completed;
+    logToDo(id, title, completed);
 });
+var logToDo = function (id, title, completed) {
+    console.log("\n    The Todo with id: ".concat(id, "\n    Has a title of: ").concat(title, "\n    Is it completed? ").concat(completed ? 'yes' : 'no', "\n  "));
+};
---
+++ variables/variables.ts
@@ -0,0 +1,21 @@
+let oranges: number = 5
+let speed: string = 'fast'
+let hasDog: boolean = true
+let nothing: null = null
+let nothing1: undefined = undefined
+
+// built-in objects
+
+let now: Date = new Date()
+
+// array
+let colors: string[] = ['red', 'green', 'blue']
+let numders: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+let results: boolean[] = [true, false, false]
+
+//classes
+class Car {
+
+}
+
+let car: Car = new Car()
